4666e6ddab157c05cdb9542bacfca48082d3791f
musnymubarak <musnymohammed@gmail.com>
2025-04-05 13:40:51 +0530

    Allow patch in cors

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin:"*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // If you need cookies or other credentials
   })
